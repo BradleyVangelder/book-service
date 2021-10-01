98e889d7f752b517fcc62a7f8691467cf32e4822
Sam Poelmans <r0790203@student.thomasmore.be>
2021-10-01 15:48:50 +0200

    db connection

--- src/main/java/fact/it/bookservice/repository/BookRepository.java
+++ src/main/java/fact/it/bookservice/repository/BookRepository.java
@@ -1,11 +1,11 @@
 package fact.it.bookservice.repository;
 
 import fact.it.bookservice.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 import java.util.List;
 
-public interface BookRepository extends JpaRepository<Book,Long> {
+public interface BookRepository extends Neo4jRepository<Book,Long> {
     List<Book> findBookByISBN(String ISBN);
     List<Book> findBookByCategory(String category);
 }
