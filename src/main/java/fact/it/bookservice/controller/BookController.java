24b27869e2b4308ee6ba8f194941011b233eb5b4
Sam Poelmans <r0790203@student.thomasmore.be>
2021-10-04 16:40:55 +0200

    Dockerfile & Dockercompose

--- src/main/java/fact/it/bookservice/controller/BookController.java
+++ src/main/java/fact/it/bookservice/controller/BookController.java
@@ -12,25 +12,39 @@ import java.util.List;
 
 @RestController
 
-
+@RequestMapping("/book")
 public class BookController {
-    private final Driver driver;
+
     private BookRepository bookRepository;
-    public BookController(Driver driver,BookRepository bookRepository){
-        this.driver = driver;
+    public BookController(BookRepository bookRepository){
+
         this.bookRepository = bookRepository;
     }
-    @PostMapping("/book")
-        public Book addBook(@RequestBody Book book){
-        bookRepository.save(book);
-        return book;
+    @PostConstruct
+    public void fillDB(){
+        if( bookRepository.count()==0){
+            bookRepository.save(new Book("Harry Potter","Finance", "687468434567"));
+            bookRepository.save(new Book("Harry Potter 2", "Finance", "687468434567"));
+        }
     }
-
-
-    @GetMapping("/")
+    @GetMapping("/book")
     public List<Book> getAllBooks(){
         List<Book> books = bookRepository.findAll();
         return books;
 
+    }
+    @GetMapping("/{ISBN}")
+    public List<Book> getBooksbyISBN(@PathVariable String ISBN){
+        return bookRepository.findBookByISBN(ISBN);
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Book> getBooksByCategory(@PathVariable String category){
+        return bookRepository.findBookByCategory(category);
+    }
+    @PostMapping("/")
+    public Book addBook(@RequestBody Book book){
+        bookRepository.save(book);
+        return book;
     }
 }
