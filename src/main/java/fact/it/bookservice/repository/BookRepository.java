3ea004bc9fe103492807b2aba15395435be20805
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-04 14:54:28 +0100

    fixing query

--- src/main/java/fact/it/bookservice/repository/BookRepository.java
+++ src/main/java/fact/it/bookservice/repository/BookRepository.java
@@ -3,13 +3,14 @@ package fact.it.bookservice.repository;
 import fact.it.bookservice.model.Book;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookRepository extends Neo4jRepository<Book,Long> {
     Book findBookByISBN(String ISBN);
     List<Book> findBookByCategory(String category);
-    @Query("select b from Book b where lower(b.title) like %lower(?1)%")
-    Book findbyTitleFree(String title);
+    @Query("select b from Book b where lower(b.title) like %lower(:titleToFind)%")
+    Book findbyTitleFree(@Param("titleToFind") String title);
     Book findBookById(Long Id);
 }
